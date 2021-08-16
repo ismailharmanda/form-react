71dc5e86bcc72fcdd13abb93ccc58ba7fc2bfbc7
ismail harmanda <iharmanda@hotmail.com>
2021-08-16 10:02:28 +0300

    get inputs to state and prevent blank submit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,26 @@
+import { useRef, useState } from "react";
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+  const fromSubmissionHandler = (event) => {
+    event.preventDefault();
+    if (enteredName.trim() === "") {
+      return;
+    }
+    setEnteredName("");
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={fromSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          value={enteredName}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
