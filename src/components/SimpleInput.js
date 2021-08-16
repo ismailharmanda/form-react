b7bbe9987823cde18dc3867f9d48da4061b2769f
ismail harmanda <iharmanda@hotmail.com>
2021-08-16 11:58:54 +0300

    overall form validity

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -5,6 +5,8 @@ const SimpleInput = (props) => {
 
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  let formIsValid = false;
+  enteredNameIsValid && (formIsValid = true);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -43,7 +45,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
